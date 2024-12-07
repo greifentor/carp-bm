fbccbd1e10e7f219444347bc5e6d78ae28f7da3c
ollie <lieshoff@gmx.de>
2024-12-08 00:09:51 +0100

    Clean up.

--- service/src/main/java/de/ollie/carp/bm/persistence/repository/ImageDBORepository.java
+++ service/src/main/java/de/ollie/carp/bm/persistence/repository/ImageDBORepository.java
@@ -1,9 +1,6 @@
 package de.ollie.carp.bm.persistence.repository;
 
 import de.ollie.carp.bm.persistence.entity.ImageDBO;
-import de.ollie.carp.bm.persistence.entity.ImageTypeDBO;
-import de.ollie.carp.bm.persistence.entity.SitzungTypDBO;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,8 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ImageDBORepository extends JpaRepository<ImageDBO, Long> {
 	Optional<ImageDBO> findByGlobalId(String globalId);
-
-	List<ImageDBO> findAllByImageType(ImageTypeDBO imageType);
-
-	List<ImageDBO> findAllBySitzungTyp(SitzungTypDBO sitzungTyp);
 }
