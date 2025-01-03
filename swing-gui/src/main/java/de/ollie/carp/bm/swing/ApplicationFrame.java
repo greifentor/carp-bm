2380f89f2fca8d1bc00c596fd38c7992817afd7d
ollie <lieshoff@gmx.de>
2025-01-03 19:40:52 +0100

    A first test implementation for showing a battle map in swing GUI.

--- swing-gui/src/main/java/de/ollie/carp/bm/swing/ApplicationFrame.java
+++ swing-gui/src/main/java/de/ollie/carp/bm/swing/ApplicationFrame.java
@@ -1,21 +1,82 @@
 package de.ollie.carp.bm.swing;
 
+import de.ollie.carp.bm.client.BattleMapClient;
+import de.ollie.carp.bm.client.TokenClient;
+import de.ollie.carp.bm.core.model.BattleMap;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 @Named
 public class ApplicationFrame extends JFrame implements WindowListener {
 
+	private static final int HGAP = 3;
+	private static final int VGAP = 3;
+
+	@Inject
+	private BattleMapClient battleMapClient;
+
+	@Inject
+	private TokenClient tokenClient;
+
 	public ApplicationFrame() {
 		super(";op");
+	}
+
+	@PostConstruct
+	void postConstruct() {
 		addWindowListener(this);
-		setSize(new Dimension(300, 100));
-		// pack();
+		setSize(new Dimension(800, 600));
+		setContentPane(createMainPanel());
 		setVisible(true);
 		requestFocus();
+	}
+
+	private JPanel createMainPanel() {
+		JPanel p = new JPanel(new BorderLayout(VGAP, HGAP));
+		p.add(BorderLayout.CENTER, new JScrollPane(createImage()));
+		return p;
+	}
+
+	private JLabel createImage() {
+		BattleMap battleMap = battleMapClient.findAllBattleMaps().get(0);
+		ImageIcon bmImage = new ImageIcon(battleMap.getImage());
+		Image img = new BufferedImage(bmImage.getIconWidth(), bmImage.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = (Graphics2D) img.getGraphics();
+		g.drawImage(bmImage.getImage(), 0, 0, null);
+
+		tokenClient
+			.findAllByBattleMap(battleMap.getName())
+			.forEach(bmt ->
+				System.out.println(
+					bmt.getToken().getName() +
+					" " +
+					bmt.getToken().getImage().length +
+					" " +
+					bmt.getPositionX() +
+					" " +
+					bmt.getPositionY()
+				)
+			);
+		tokenClient
+			.findAllByBattleMap(battleMap.getName())
+			.forEach(bmt ->
+				g.drawImage(new ImageIcon(bmt.getToken().getImage()).getImage(), bmt.getPositionX(), bmt.getPositionY(), null)
+			);
+		ImageIcon imageIcon = new ImageIcon(img);
+		return new JLabel(imageIcon);
 	}
 
 	@Override
@@ -25,12 +86,14 @@ public class ApplicationFrame extends JFrame implements WindowListener {
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		System.out.println("Closed");
 		// NOP
 	}
 
 	@Override
-	public void windowClosing(WindowEvent e) {}
+	public void windowClosing(WindowEvent e) {
+		System.out.println("Closed");
+		dispose();
+	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
