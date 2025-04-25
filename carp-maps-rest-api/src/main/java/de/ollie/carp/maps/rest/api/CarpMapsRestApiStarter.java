30f9569dc9f4d506c6f926cc3312d9963289da36
ollie <lieshoff@gmx.de>
2025-04-26 00:00:04 +0200

    Added a sub project for a CARP maps REST API.

---
+++ carp-maps-rest-api/src/main/java/de/ollie/carp/maps/rest/api/CarpMapsRestApiStarter.java
@@ -0,0 +1,20 @@
+package de.ollie.carp.maps.rest.api;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Generated
+@SpringBootApplication
+@ComponentScan("de.ollie.carp.maps.rest.api")
+@EntityScan("de.ollie.carp.maps.rest.api.persistence.entity")
+@EnableJpaRepositories(basePackages = { "de.ollie.carp.maps.rest.api.persistence.repository" })
+public class CarpMapsRestApiStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpMapsRestApiStarter.class, args);
+	}
+}
