fd2b42ca2d7653895b153aec13265b196e89a7be
ollie <lieshoff@gmx.de>
2025-05-02 17:55:08 +0200

    Corrected integration test.

--- carp-maps-rest-api/src/test/java/de/ollie/carp/maps/rest/api/rest/v1/TokenControllerITest.java
+++ carp-maps-rest-api/src/test/java/de/ollie/carp/maps/rest/api/rest/v1/TokenControllerITest.java
@@ -11,23 +11,28 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = CarpMapsRestApiStarter.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TokenControllerITest {
+
+	private static final String VALID_TOKEN =
+		"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJjYXJwLWJtIiwidWlkIjoidWlkIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjkxNTE2MjM5MTIyfQ.CI-raSXUHcKxIqs0PnZRXLPop7ipB_oeoT9cndsJq-WN71_5pGX5jMDOrzecIPBJX3AMy26lFtMXmveWoAzJTA";
 
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
-	void t() throws Exception {
+	void happyRun() throws Exception {
 		mvc
 			.perform(
 				get("/api/v1/tokens?page=1")
-					.header(HttpHeaders.AUTHORIZATION, "Bearer ;op")
+					.header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
 					.contentType(MediaType.APPLICATION_JSON)
 			)
 			.andExpect(status().isOk());
